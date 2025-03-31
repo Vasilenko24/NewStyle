0ba9b9e3893e18c042aa931ef0d37eb75c4ef153
Slavik24V <restylezp@gmail.com>
2025-03-31 06:59:42 +0300

    dropdown list

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function header () {
+  const [showDropDown, setShowDropDown] = useState(false)
     return (
             <header className="flex items-center justify-between px-6 py-4">
               <Link href="/">
@@ -15,8 +18,29 @@ export default function header () {
                 />
               </Link>
         
-              <nav className="flex gap-6">
-                <Link href="/">Test</Link>
+              <nav className="flex gap-6 relative">
+                <Link href="/">Home</Link>
+
+          <div className="relative group">
+          <button className="hover:underline flex items-center gap-1 mb-2">
+            Our services
+            <span className="text-xs">▼</span>
+          </button>
+
+          <div className="absolute left-0  bg-white text-black rounded shadow-md z-10 hidden group-hover:flex flex-col min-w-[200px]">
+            <Link href="/auto-tinting" className="block px-4 py-2 hover:bg-gray-200">
+              Auto Window Tinting
+            </Link>
+            <Link href="/residential-tinting" className="block px-4 py-2 hover:bg-gray-200">
+              Residential Window Tinting
+            </Link>
+            <Link href="/commercial-tinting" className="block px-4 py-2 hover:bg-gray-200">
+              Commercial Window Tinting
+            </Link>
+          </div>
+        </div>
+                <Link href="/">Maybe somth else</Link>
+                <Link href="/">Contact us</Link>
               </nav>
             </header>
     )
